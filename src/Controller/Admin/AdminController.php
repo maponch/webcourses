8a1e3afe29f62f6944572abb25dc6c06880e0198
maponch <ma.ponchelet@gmail.com>
2025-01-12 16:46:29 +0100

    envoie du formulaire de creation de cours mais ne fonctionne pas car pas encore d'authetification

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Course;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 use App\Form\CourseType;
+use Cocur\Slugify\Slugify;
 
 
 class AdminController extends AbstractController
@@ -19,11 +23,28 @@ class AdminController extends AbstractController
         ]);
     }
     #[Route('/admin/newcourse', name: 'app_admin_newcourse')]
-    public function newCourse(): Response
+    public function newCourse(Request $request, EntityManagerInterface $manager): Response
     {
-        $form = $this->createForm(CourseType::class);
+        $user = $this->getUser();
+        if (!$user) {
+            dd($user);
+        }
+        $course = new Course();
+        $slugify = new Slugify();
+        $form = $this->createForm(CourseType::class, $course);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()&&$form->isValid()){
+            $course->setUser($this->getUser())
+                ->setCreatedAt(new \DateTimeImmutable())
+                ->setSlug($slugify->slugify($course->getName()));
+            $manager->persist($course);
+            $manager->flush();
+            return $this->redirectToRoute('app_admin');
+        }
         return $this->render('admin/newcourse.html.twig', [
             'form' => $form,
         ]);
+
     }
 }
--- src/Form/CourseType.php
+++ src/Form/CourseType.php
@@ -8,6 +8,10 @@ use App\Entity\Level;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,29 +20,31 @@ class CourseType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('duration')
-            ->add('price')
-            ->add('created_at', null, [
-                'widget' => 'single_text',
+            ->add('name', TextType::class, [
+                'label' => 'Name of course',
             ])
-            ->add('slug')
+            ->add('description', TextareaType::class, [
+                'label' => 'Course description',
+            ])
+            ->add('duration', TextType::class, [
+                'label' => 'Duration'
+            ])
+            ->add('price', IntegerType::class, [
+                'label' => 'Price'
+            ])
             ->add('image')
             ->add('program')
             ->add('category', EntityType::class, [
                 'class' => Category::class,
-                'choice_label' => 'id',
+                'choice_label' => 'name',
             ])
             ->add('level', EntityType::class, [
                 'class' => Level::class,
-                'choice_label' => 'id',
+                'choice_label' => 'name',
             ])
-            ->add('user', EntityType::class, [
-                'class' => User::class,
-                'choice_label' => 'id',
-            ])
-        ;
+            ->add('save', SubmitType::class, [
+                'label' => 'Ajouter'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
